6521c9807bc037bf02d8466baebe8ebeabad22e7
bethebiggreen <bethebiggreen@gmail.com>
2016-03-15 14:45:57 +0900

    A branch for 'Elevator'

--- Elevator/Elevator_main.cpp
+++ Elevator/Elevator_main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	printf("happy");
+	freopen("input.txt", "r", stdin);
+	int  start, end, total, up, down;
+	cin >> total >> start >> end >> up >> down;
+
+	if (start > end) {
+		int go_up = end / up;
+		if ((go_up + 1)*up > total)
+			cout << "use the stairs" << endl;
+
+		int remain = (go_up + 1)*up - total;
+
+	}
+	else {
+		cout << "happy hacking !!" << endl;
+	}
+
 	return 0;
 }
