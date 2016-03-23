f60821e3a5f3fc2c0380d0c9f46438d44606b8c3
bethebiggreen <bethebiggreen@gmail.com>
2016-03-24 00:23:38 +0900

    flash_move : simple algorithm is implemented.

---
+++ FlashMove/FlashMove.cpp
@@ -0,0 +1,68 @@
+/*
+
+0.01 https://www.acmicpc.net/problem/1697
+     Initial algorithm describe below.
+	 17/2 = 8.5 = 8 -> + 2 times (if no remainder, we add 1 times only)
+	 8/2 = 4 -> + 1 times.
+	 5 to 4 -> +1 times
+	 Totally, 4 times.
+	 Every time, the calculation that check half of number is smaller than target.
+	 And if it is smaller, check, how small it is.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int a = 0 ;
+int b = 0 ;
+int sol = 0;
+
+void input_proc(void)
+{
+	freopen("input.txt", "r", stdin);
+	cin >> a >> b;
+}
+
+void do_something(void)
+{
+	int tmp = 0;
+	if (a > b) {
+		tmp = a;
+		a = b;
+		b = tmp;
+	}
+
+	//float half = (float)b;
+
+	while (b/2 > a)
+	{
+		if (b % 2)
+			sol += 2;
+		else
+			sol++;
+		b /= 2;		
+	}
+
+	bool is_even = false;
+	if (b % 2 == 0)
+		is_even = true;
+
+	int case1 = a - (b / 2 + is_even) + is_even + 1;
+	int case2 = b - a;
+	
+	sol += (case1 > case2) ? case2 : case1;
+}
+
+void output_proc(void)
+{
+	cout << sol;
+}
+
+int main(void)
+{
+	input_proc();
+	do_something();
+	output_proc();
+	return 0;
+}
