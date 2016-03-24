260256fe45c31ad7b7d1a50bd2d77469be57cd50
bethebiggreen <bethebiggreen@gmail.com>
2016-03-24 16:44:10 +0900

    DFS is not proper for this issue.

--- FlashMove/FlashMove.cpp
+++ FlashMove/FlashMove.cpp
@@ -1,5 +1,7 @@
 /*
 
+0.02 add traversing all cases with hueristic answer.
+
 0.01 https://www.acmicpc.net/problem/1697
      Initial algorithm describe below.
 	 17/2 = 8.5 = 8 -> + 2 times (if no remainder, we add 1 times only)
@@ -14,27 +16,53 @@
 
 using namespace std;
 
-int a = 0 ;
-int b = 0 ;
+int g_a = 0 ;
+int g_b = 0 ;
 int sol = 0;
 
 void input_proc(void)
 {
 	freopen("input.txt", "r", stdin);
-	cin >> a >> b;
+	cin >> g_a >> g_b;
 }
 
+int calls_of_traverse = 0;
+int dx[2] = { -1,1 };
+void traverse(int x, int times)
+{
+	/*
+	if (times > sol)
+		return;
+		*/
+	calls_of_traverse++;
+	cout << "calls_of_traverse:" << calls_of_traverse << ", x:" << x << endl;
+
+	if (x < 0)
+		return;
+
+	if (x == g_b) {
+		if (sol > times)
+			sol = times;
+		return;
+	}
+
+	// -1, 1 and x2
+	for (int i = 0; i < 2; i++) {
+		traverse(x + dx[i], times + 1);
+	}
+	traverse(2 * x, times + 1);
+}
 void do_something(void)
 {
 	int tmp = 0;
-	if (a > b) {
-		tmp = a;
-		a = b;
-		b = tmp;
+	if (g_a > g_b) {
+		tmp = g_a;
+		g_a = g_b;
+		g_b = tmp;
 	}
 
-	//float half = (float)b;
-
+	int a = g_a;
+	int b = g_b;
 	while (b/2 > a)
 	{
 		if (b % 2)
@@ -52,11 +80,15 @@ void do_something(void)
 	int case2 = b - a;
 	
 	sol += (case1 > case2) ? case2 : case1;
+
+	traverse(g_a, 0);
 }
+
 
 void output_proc(void)
 {
 	cout << sol;
+	cout << endl << calls_of_traverse << endl;
 }
 
 int main(void)
