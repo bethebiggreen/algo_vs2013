1441353e8545c93525c3c2be7f6d47fda0dbc931
bethebiggreen <bethebiggreen@gmail.com>
2016-03-26 10:45:30 +0900

    safe_region : checked recirsive-calls are not called.

--- SafetRegion/SafeRegion.cpp
+++ SafetRegion/SafeRegion.cpp
@@ -1,5 +1,6 @@
 /*
 
+  0.03 : Confirmed unnecessary recursive-calls are not called. #include <queue> is needed, further investigation.
   0.02 : Timeout is occured after implementing of BFS.
   0.01 : https://www.acmicpc.net/problem/2468
          BFS is adjustd to traverse adjacent verticies.
@@ -8,12 +9,16 @@
 
 #include <iostream>
 #include <cstdio>
-
+#if _DEBUG
+#include <stdio.h>
+#include <cstdlib>
+#endif
 using namespace std;
 
 int N = 0;
 const int MAX_N = 100+2;
 int g_map[MAX_N][MAX_N];
+int local_map[MAX_N][MAX_N];
 int sol = 0;
 
 enum status {
@@ -44,10 +49,16 @@ node queue[MAX_QUEUE_SIZE];
 int queue_begin = 0;
 int queue_end = -1;
 
+FILE* fp = NULL;
 inline void add_queue(int i, int j)
 {
 #if _DEBUG
-	cout << "i:" << i << ", j:" << j << ", queue_end:" << queue_end << endl;
+	//cout << "i:" << i << ", j:" << j << ", queue_end:" << queue_end << endl;
+	
+	if (fp)
+		fprintf(fp, "[%15s] i:%d, j:%d, queue_end:%d\n", __FUNCTION__, i, j, queue_end);
+	else
+		cout << "fopen is failed" << endl;
 #endif
 	queue[++queue_end] = node(i, j);
 }
@@ -56,7 +67,7 @@ inline node pop_queue(void)
 {
 	if (queue_begin > queue_end)
 		return node(-1, -1);
-
+	fprintf(fp, "[%15s] i:%d, j:%d, queue_begin:%d\n", __FUNCTION__, queue[queue_begin].i, queue[queue_begin].j, queue_begin);
 	return queue[queue_begin++];
 }
 
@@ -95,12 +106,15 @@ int flood(int depth)
 }
 
 
-inline void mark(int i, int j, int map[MAX_N][MAX_N])
+inline void mark(int i, int j)
 {
-	if (map[i][j] == FLOODED)
+	fprintf(fp, "[%15s] i:%d, j:%d will add belows.\n", __FUNCTION__,i, j);
+	if (local_map[i][j] == FLOODED) {
+		fprintf(fp, "[%15s] i:%d, j:%d is returned with out adding.\n", __FUNCTION__, i, j);
 		return;
+	}
 	else
-		map[i][j] = FLOODED;
+		local_map[i][j] = FLOODED;
 
 	for (int dir = 0; dir < 4; dir++) {
 		int new_i = i + dx[dir];
@@ -108,7 +122,7 @@ inline void mark(int i, int j, int map[MAX_N][MAX_N])
 
 		if (new_i < 0 || new_j < 0 || new_i >= N || new_j >= N)
 			continue;
-		if(map[new_i][new_j] > 0)
+		if(local_map[new_i][new_j] > 0)
 			add_queue(new_i, new_j);
 	}
 }
@@ -130,31 +144,39 @@ void init_queue(void)
 	queue_end = -1;
 }
 
+
+
 int num_of_safe_region(int start, const int m[MAX_N][MAX_N])
 {
 
 	int safe_region = 0;
-	int local_map[MAX_N][MAX_N];
+
 	copy_map(local_map, m);
 	
 	while (1) {
 		init_queue();
 		bool found = false;
+		fprintf(fp, "[%15s] finding start begins\n", __FUNCTION__);
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < N; j++)
 				if (local_map[i][j] > 0) {
+					fprintf(fp, "[%15s] i:%d j:%d will be added\n", __FUNCTION__, i, j);
 					add_queue(i, j);
 					i = j = N;
 					found = true;
 					safe_region++;
 				}
+		fprintf(fp, "[%15s] finding start ends\n", __FUNCTION__);
+
 		if (found) {
+			fprintf(fp, "[%15s] marking begins\n", __FUNCTION__);
 			while (1) {
 				node n = pop_queue();
 				if (n.i == -1)
 					break;
-				mark(n.i, n.j, local_map);
+				mark(n.i, n.j);
 			}
+			fprintf(fp, "[%15s] marking ends\n", __FUNCTION__);
 		}
 		else
 			break;
@@ -199,8 +221,12 @@ void output_proc()
 
 int main()
 {
+	fp = fopen("output.txt", "w");
 	input_proc();
+
 	do_something();
 	output_proc();
+	if (fp)
+		fclose(fp);
 	return 0;
 }
