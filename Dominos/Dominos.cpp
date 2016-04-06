44f15c21640ae97a55d3c22aa71b82cc6851051c
bethebiggreen <bethebiggreen@gmail.com>
2016-04-06 15:04:29 +0900

    dominos : samples are okay to pass.

--- Dominos/Dominos.cpp
+++ Dominos/Dominos.cpp
@@ -1,7 +1,9 @@
 #include <cstdio>
 #include <iostream>
 
+#define __USE_DYNAMIC_MEMORY_ALLOCATION__ 1
 using namespace std;
+
 typedef enum node_type_e{
 	VERTICAL = 0,
 	HORIZONTAL = 1,
@@ -12,36 +14,29 @@ typedef struct node_t {
 	int x1, y1, x2, y2;
 	node_type type;
 	node_t(int in_val = -1, int in_x1 = -1, int in_y1 = -1, int in_x2 = -1, int in_y2 = -1, node_type in_type = VERTICAL) 
-		: val(in_val), x1(in_x1), y1(in_y1), x2(in_x2), y2(in_y2) , type(in_type)
-	{}
-
+		: val(in_val), x1(in_x1), y1(in_y1), x2(in_x2), y2(in_y2) , type(in_type) {}
 } node ;
 
-
-int TC_NUM = 0;
 const int MAX_N = 2000;
-int MAP[MAX_N][MAX_N];
-int N = 0, K = 0;
 int MAX_NODE_SIZE = 0;
+const int MAX_DEPTH = 30;
+int MAP[MAX_N][MAX_N] = { 0, };
+bool VISITED[MAX_N][MAX_N] = { 0, };
+int N = 0, K = 0;
+int SUM = 0;
+int solution = 0;
 
-// node* node_by_big_order; // Why 30 ?
-node node_by_big_order[50]; // Why 30 ?
+#if __USE_DYNAMIC_MEMORY_ALLOCATION__
+node* node_by_big_order = NULL; 
+#else
+node node_by_big_order[50];
+#endif 
+
 int node_by_big_order_pos = 0;
 
-inline bool is_bigger(const node& a, const node& b)
-{
-	return (a.val > b.val) ? true : false;
-}
-
-inline bool is_smaller(const node& a, const node& b)
-{
-	return (a.val > b.val) ? false : true;
-}
-
-inline bool is_equal(const node& a, const node& b)
-{
-	return (a.val == b.val) ? true : false;
-}
+inline bool is_bigger(const node& a, const node& b)  { return (a.val > b.val)  ? true : false; }
+inline bool is_smaller(const node& a, const node& b) { return (a.val > b.val)  ? false : true; }
+inline bool is_equal(const node& a, const node& b)   { return (a.val == b.val) ? true : false; }
 
 inline bool swap(node& a, node& b)
 {
@@ -51,47 +46,15 @@ inline bool swap(node& a, node& b)
 	return true;
 }
 
-// left -> greater
-// right -> lesser 
-// -> equal to 
-//           p           l    r      
-//    -1 ,   -1,   -1,   0 , -1
 void quick_sort(int l, int r, node* n)
 {
 	if (l >= r)
 		return;
 
-	//static node* n = node_by_big_order;
-
 	int pivot = l;
 	int start = l + 1;
 	node pivot_node = n[pivot];
 	int end = r;
-
-	// For covering all cases!
-	// Legacy algorithm cares only bigger and lesser, so algorithm does not have clear idea for same value as pivot.
-	// Goal : Every try makes a partitione like below
-	//       | Lesser and equal to PIVOT |  PIVOT  | Greater than PIVOT 
-	// 1. From left, two things are stop-triggers. Otherwise, start will be increased.
-	//   1) Once it meets greater number.
-	//   2) Once 'start', which is assigned from left initially, is over 'end'.
-	// 2. From right, three things are stop-triggers.
-	//   1) Once it meets lesser number.
-	//   2) Once 'end', which is assigned from right initially, is under 'start'.
-	//   3) Once it meets pivot. However this case, 'end' should be decreased to avoid dead-lock.
-	//      (Our pivot is always next to left, so we could conclude while loop after it.) 
-	// 
-	// For swapping cases
-	// 1. 'start' is over 'end'.
-	//  1) 'end' swaps with 'pivot'.
-	// 2. Otherwise,
-	//  1) 'start' is far from 'end'. We would swap between 'start' and 'end'.
-	//  2) 'start' is same as 'end'. It would swap each other, however there is nothing changed in a conclusion.
-	//     After this, the loop will be broken. 
-	//  3) 'start' is over 'end'. We would swap between 'end' and 'pivot', and next recursion is expected.
-	//
-	// * Aware that over and under are not including 'equal to'.
-    // * Swapping 'is_smaller' and 'is_bigger' effects in-versa- ordering.
 
 	while (start < end) {
 		while (start <= end) {
@@ -124,71 +87,131 @@ void quick_sort(int l, int r, node* n)
 void input_proc(void)
 {
 	cin >> N >> K;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			cin >> MAP[i][j];
+			SUM += MAP[i][j];
+		}
+	}
 	
 	MAX_NODE_SIZE = 2*(2*((N - 1)*(N - 1)) + 2*(N - 1));
-	// node_by_big_order = new node[MAX_NODE_SIZE];
+#if __USE_DYNAMIC_MEMORY_ALLOCATION__
+	node_by_big_order = new node[MAX_NODE_SIZE];
+#endif
 }
-
 
 inline bool add_node(int y, int x, node_type type)
 {
+	// We will add only bigger node than smallest one, at least.
+	const int smallest = node_by_big_order[node_by_big_order_pos - 1].val;
 	switch (type) {
 	case HORIZONTAL:
-		if (x+1 < N - 1 && y < N - 1) 
-			node_by_big_order[node_by_big_order_pos++] = { MAP[y][x] + MAP[y][x + 1], x, y, x+1, y, type };
+		if (x + 1 < N) {
+			if ((MAP[y][x] + MAP[y][x + 1]) < smallest )
+				return false;
+			node_by_big_order[node_by_big_order_pos++] = { MAP[y][x] + MAP[y][x + 1], x, y, x + 1, y, type };
+		}
 		break;
 	case VERTICAL:
-		if (x < N - 1 && y + 1 < N - 1)
-			node_by_big_order[node_by_big_order_pos++] = { MAP[y][x] + MAP[y+1][x], x, y, x, y+1, type };
+		if (y + 1 < N) {
+			if (MAP[y][x] + MAP[y + 1][x] < smallest)
+				return false;
+			node_by_big_order[node_by_big_order_pos++] = { MAP[y][x] + MAP[y + 1][x], x, y, x, y + 1, type };
+		}
 		break;
 	default:
 		break;
 	}
+
+	quick_sort(0, node_by_big_order_pos-1, node_by_big_order);
 	return true;
+}
+
+inline bool can_it_put(const node& n)
+{
+	if (VISITED[n.y1][n.x1] || VISITED[n.y2][n.x2]) 
+		return false;
+	else
+		return true;
+}
+
+inline bool mark(const node& n)
+{
+	if (can_it_put(n)) {
+		VISITED[n.y1][n.x1] = true;
+		VISITED[n.y2][n.x2] = true;
+		return true;
+	}
+	else
+		return false;
+}
+
+inline bool unmark(const node& n)
+{
+	if (!can_it_put(n)) {
+		VISITED[n.y1][n.x1] = false;
+		VISITED[n.y2][n.x2] = false;
+		return true;
+	}
+	else
+		return false;
+}
+
+void find_max(int value, int num_dominos, int depth)
+{
+	if (!num_dominos || depth >= MAX_DEPTH) {
+		if (solution < value)
+			solution = value;
+		return;
+	}
+
+	const node& n = node_by_big_order[depth];
+	if (can_it_put(n)) {
+		mark(n);
+		find_max(value + n.val, num_dominos - 1, depth + 1);
+		unmark(n);
+		find_max(value, num_dominos, depth + 1);
+	}
+	else {
+		find_max(value, num_dominos, depth + 1);
+	}
 }
 
 // Heap can be the best solution for it due to save the memory.
 void do_something(void)
 {
-	// Make all coup
+	// Make all couples
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < N; j++)
 			for (int type = VERTICAL; type <= HORIZONTAL; type++)
 				add_node(i, j, (node_type)type);
 
-
-	node a[5] = { {-1,0,0,0,0},
-				  { 0,0,0,0,0},
-				  {-1,0,0,0,0},
-				  {-1,0,0,0,0},
-				  {-1,0,0,0,0} };
-	// Sort by order
-	quick_sort(0, MAX_NODE_SIZE/2 - 1, node_by_big_order);
-
-
+	find_max(0,K,0);
 }
 
 void reset(void)
 {
-	/*
+#if __USE_DYNAMIC_MEMORY_ALLOCATION__
 	if (node_by_big_order)
 		delete[] node_by_big_order;
 	node_by_big_order = NULL;
-	*/
+#endif
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
 			MAP[i][j] = -1;
+			VISITED[i][j] = false;
+		}
+	}
+	
 	MAX_NODE_SIZE = 0;
+	solution = 0;
+	SUM = 0;
 }
-
 
 void output_proc(void)
 {
-
+	printf("%d\n", SUM - solution);
 }
 
 int main()
@@ -196,7 +219,7 @@ int main()
 #if _DEBUG
 	freopen("input.txt", "r", stdin);
 #endif
-	int tc_num = 0;
+	int TC_NUM = 0;
 	cin >> TC_NUM;
 	while (TC_NUM--) {
 		input_proc();
