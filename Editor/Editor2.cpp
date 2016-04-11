5dc04bdeaacaa47bb14f9ee151b7c2d7a26abf75
bethebiggreen <bethebiggreen@gmail.com>
2016-04-11 14:55:33 +0900

    dp is never stopped : )

--- Editor/Editor2.cpp
+++ Editor/Editor2.cpp
@@ -7,17 +7,7 @@ using namespace std;
 stack<char> input;
 stack<char> output;
 
-void input_proc(void)
-{
-
-}
-void do_something(void);
-void output_proc(void);
-
 int main(void)
 {
-	input_proc();
-	do_something();
-	output_proc();
 	return 0;
 }
