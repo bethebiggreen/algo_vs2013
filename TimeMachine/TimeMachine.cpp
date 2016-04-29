1723690873150c872bf7ced1a6b4665663e952ad
bethebiggreen <bethebiggreen@gmail.com>
2016-04-29 14:37:54 +0900

    time_machine : judge_failed

--- TimeMachine/TimeMachine.cpp
+++ TimeMachine/TimeMachine.cpp
@@ -1,58 +1,105 @@
+/*
+3 4
+1 2 4
+1 3 3
+2 3 -1
+3 1 -2
+*/
 #include <stdio.h>
+const int MAX_N = 500;
 
+int arr[MAX_N][MAX_N];
+int dist[MAX_N];
+int at_least_once[MAX_N];
+// bool connected[MAX_N][MAX_N];
+int connected[MAX_N][MAX_N];
+int N = 0;
 
-int arr[7] = { -1,1,1,-1,-1,1,1 };
-int sort[7] = { 0, };
-void merge_sort(int l, int r)
-{
-	if (l == r)
-		return;
-
-	int mid = (l + r) / 2;
-	merge_sort(l, mid);
-	merge_sort(mid + 1, r);
-	printf("%d %d\n", l, r);
-	int p = l;
-	int q = mid+1;
-	int cnt = p;
-
-	while (p <= mid+1 && q <= r && cnt <= r) {
-		if (arr[p] <= arr[q])
-			sort[cnt++] = arr[p++];
-		else
-			sort[cnt++] = arr[q++];
-	}
-	while (cnt <= r) {
-		if (p <= mid + 1) 
-			sort[cnt++] = arr[p++];
-		else 
-			sort[cnt++] = arr[q++];
-	}
-
-	for (int i = l; i <= r; i++)
-		arr[i] = sort[i];
-
-}
-
+// const int MAX_INT = 0x7FFF;
+// const int MIN_INT = 0xFFFF;
+const int MAX_INT = 10001;
+const int MIN_INT = -10001;
 
 void input_proc(void)
 {
+	int edge_num = 0;
+	scanf("%d %d\n", &N, &edge_num);
+	for (int i = 0; i < N; i++) {
+		dist[i] = MAX_INT;
+		at_least_once[i] = false;
+		for (int j = 0; j < N; j++)
+			arr[i][j] = MAX_INT;
+	}
+
+	int u, v, val;
+	for (int i = 0; i < edge_num; i++) {
+		scanf("%d %d %d\n", &u, &v, &val);
+		u--, v--;
+		if (arr[u][v] > val)
+			arr[u][v] = val;
+		if (!connected[u][v])
+			connected[u][v] = true;
+	}
 
 }
 
-void do_something(void)
+bool do_something(void)
 {
+	/*
+	This loop finds MIN dist from assigned vertex to others. It runs until num of |V-1|.
+	*/
+	// We consider ith vertex.
+	int base = 0;
+	dist[base] = 0;
+	at_least_once[0] = true;
+	for (int it = 0; it < N - 1; it++) {
+		// And iterate it others.
+		for (int i = 0; i < N ; i++) {
+			for (int j = 0; j < N; j++) {
+				if (connected[i][j]) {
+					if (!at_least_once[j] && at_least_once[i]) {
+						dist[j] = dist[i] + arr[i][j];
+						at_least_once[j] = true;
+						continue;
+					} 
+					if (dist[j] > dist[i] + arr[i][j])
+						dist[j] = dist[i] + arr[i][j];
+				}
+			}
+		}
+	}
 
+	// Once more for a cycling check
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			/*
+			if (i == j)
+				continue;
+				*/
+			if (connected[i][j]) {
+				if (dist[j] > dist[i] + arr[i][j])
+					return false;
+			}
+		}
+	}
+	return true;
 }
 
 void output_proc(void)
 {
-
+	for (int i = 1; i < N; i++)
+		printf("%d\n", (at_least_once[i]) ? dist[i] : -1) ;
 }
 
 int main(void)
 {
-	merge_sort(0, 6);
-	
+#if _DEBUG
+	freopen("input.txt", "r", stdin);
+#endif
+	input_proc();
+	if (do_something())
+		output_proc();
+	else
+		printf("-1\n");
 	return 0;
 }
